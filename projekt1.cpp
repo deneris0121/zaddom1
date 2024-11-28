307fa5002516c788debb5174b9d2d612e6da2a4b
Michal Urbanczyk <deneris0121@gmail.com>
2024-11-28 12:33:03 +0100

    dodanie silni do programu

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+int silnia(int a)
+{
+    int wyn=1;
+    for(int i=2;i<=a;i++)
+    {
+        wyn=wyn*i;
+    }
+    return wyn;
+}
+
 int main() {
     int a, b;
     cout << "Podaj liczbe calkowita ";
@@ -13,7 +23,12 @@ int main() {
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
         cout << "0. Wyjscie" << endl;
+        cout << "1. Silnia" << endl;
         cin >> wyjscie;
+        if(wyjscie==1)
+        {
+            cout<<"Wynik silni dla liczby: "<<a<<" wynosi: "<<silnia(a)<<endl;
+        }
     } while(wyjscie != 0);
     return 0;
 }
