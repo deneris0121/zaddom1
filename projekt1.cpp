de7960a46a25d1965b27f0e6782b72686244c009
Michal Urbanczyk <deneris0121@gmail.com>
2024-11-28 12:58:52 +0100

    funkcja sprawdzajaca liczby pierwsze

--- projekt1.cpp
+++ projekt1.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+vector<bool> tab={0,0,1};
+
+void powieksz(int a)
+{
+    for(int i=tab.size();i<=a;i++)
+    {
+        tab.push_back(1);
+    }
+    for(int i=2;i<tab.size();i++)
+    {
+        if(tab[i]==1)
+        {
+            for(int j=2*i;j<=a;j+=i)
+            {
+                if(tab[j]==1) tab[j]=0;
+            }
+        }
+    }
+}
+
+bool pierwsza(int a)
+{
+    if(a>=tab.size())
+    {
+        powieksz(a);
+    }
+    return tab[a];
+}
+
 int main() {
     int a, b;
+    cout<<tab.size();
     cout << "Podaj liczbe calkowita ";
     cin >> a >> b;
     int wyjscie;
